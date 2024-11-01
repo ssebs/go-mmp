0d68464d3ecba40a7736ab0b9e5b8b75d1af1202
Sebastian Safari <ssebs@ymail.com>
2024-10-31 18:14:08 -0700

    working saveconfig

---
+++ config/cli_flags.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	flag "github.com/spf13/pflag"
+)
+
+// Will be used as /home/user/mmpConfig.yml, or C:\Users\user\mmpConfig.yml
+const ConfigPathShortName string = "mmpConfig.yml"
+
+// CLI flag values will be stored in this
+type CLIFlags struct {
+	GUIMode     GUIMode
+	ConfigPath  string
+	ResetConfig bool
+	Testing     bool
+}
+
+// parseFlags will parse the CLI flags that may have been used.
+// Useful for disabling the serial listening functionality
+func ParseFlags() *CLIFlags {
+	cliFlags := &CLIFlags{}
+
+	flag.VarP(&cliFlags.GUIMode, "mode", "m",
+		"GUI Mode, defaults to 'NORMAL', use 'GUIOnly' to run without a serial device.")
+	flag.BoolVarP(&cliFlags.ResetConfig, "reset-config", "r", false,
+		"Reset your ~/mmpConfig.yml file to default. If using config-path, reset that file.")
+	flag.StringVarP(&cliFlags.ConfigPath, "path", "p", ConfigPathShortName,
+		"Path to your mmpConfig.yml. If used with reset-config, the specifified file will be reset.")
+	flag.BoolVarP(&cliFlags.Testing, "dry-run", "n", false,
+		"Enable dry run / test mode")
+	// TODO: implement verbose flag
+
+	flag.Parse()
+	return cliFlags
+}
---
+++ config/gui_mode.go
@@ -0,0 +1,74 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type GUIMode int
+
+// var _ yaml.Marshaler = (*GUIMode)(nil)
+var _ yaml.Unmarshaler = (*GUIMode)(nil)
+
+const (
+	NOTSET GUIMode = iota // For use in comparing cli flags
+	NORMAL                // Serial listener + GUI
+	GUIOnly
+	// CLIOnly
+	TESTING
+)
+
+/* GUIMode pflag.Value implementation */
+func (g *GUIMode) String() string {
+	return g.Type()
+}
+func (g *GUIMode) Type() string {
+	switch *g {
+	case NORMAL:
+		return "NORMAL"
+	case GUIOnly:
+		return "GUIOnly"
+	}
+	return ""
+}
+func (g *GUIMode) Set(m string) error {
+	switch strings.ToUpper(m) {
+	case "NORMAL":
+		*g = NORMAL
+		return nil
+	case "GUIONLY":
+		*g = GUIOnly
+		return nil
+	}
+
+	return fmt.Errorf("could not find mode %s", m)
+}
+
+func (g *GUIMode) UnmarshalYAML(node *yaml.Node) error {
+	var modeStr string
+	var modeInt int
+
+	// Check if the node contains an integer
+	if err := node.Decode(&modeInt); err == nil {
+		*g = GUIMode(modeInt)
+		return nil
+	}
+
+	// If not an integer, attempt to decode as a string
+	if err := node.Decode(&modeStr); err != nil {
+		return err
+	}
+
+	// Use the Set method to handle string-based GUIMode values
+	if err := g.Set(modeStr); err != nil {
+		return fmt.Errorf("failed to unmarshal GUIMode: %w", err)
+	}
+
+	return nil
+}
+
+func (g GUIMode) MarshalYAML() (interface{}, error) {
+	return g.Type(), nil
+}
