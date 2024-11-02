3a8897a5b7bfa69fb149084646a0d772023bd855
Sebastian Safari <ssebs@ymail.com>
2024-11-01 21:38:40 -0700

    alright

--- widgets/macro_edit_box.go
+++ widgets/macro_edit_box.go
@@ -7,9 +7,32 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 	"github.com/ssebs/go-mmp/config"
 )
+
+/*
+Main gui
+- VBox:
+	- Label: Edit (macroname)
+	- Hbox:
+		- Label (name)
+		- Entry for (macroname)
+	- Label: (Actions)
+	- List:
+		- <See below>
+	- HBox:
+		- Undo btn?
+		- +NewAction btn
+		- Save btn
+- List for actions: (each item)
+	- Hbox:
+		- icon for drag/moving position
+		- Select for action
+		- entry for action param
+		- x icon for delete
+*/
 
 // Ensure interface implementation.
 var _ fyne.Widget = (*MacroEditBox)(nil)
@@ -51,7 +74,21 @@ func (eb *MacroEditBox) CreateRenderer() fyne.WidgetRenderer {
 
 func (eb *MacroEditBox) runActionEditorWindow() error {
 	newWin := eb.app.NewWindow("Edit Actions")
-	newWin.SetContent(widget.NewLabel("test"))
+	macronameentrybinding := binding.NewString()
+
+	replace_me_name := widget.NewEntryWithData(macronameentrybinding)
+	replace_me_name.PlaceHolder = eb.Macro.Name
+
+	newWin.SetContent(container.NewVBox(
+		widget.NewLabel(fmt.Sprintf("Edit %s", eb.Macro.Name)),
+		container.NewHBox(
+			widget.NewLabel("Name:"),
+			replace_me_name,
+		),
+		widget.NewButton("JOSE LOOK HERE", func() {
+			fmt.Println("SAVED", replace_me_name.Text)
+		}),
+	))
 	newWin.CenterOnScreen()
 	newWin.Show()
 	return nil
