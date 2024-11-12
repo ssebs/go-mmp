3c6176e1ef353a9f6570a9ed3b9a5634a9376494
Sebastian Safari <ssebs@ymail.com>
2024-11-11 21:49:37 -0800

    more tests

--- models/tests/guimode_test.go
+++ models/tests/guimode_test.go
@@ -10,13 +10,25 @@ import (
 func TestGUIModeModel(t *testing.T) {
 	got := models.NOTSET
 
-	got.Set("NORMAL")
-	assert.Equal(t, models.NORMAL, got)
+	t.Run("Test Set", func(t *testing.T) {
+		got.Set("NORMAL")
+		assert.Equal(t, models.NORMAL, got)
+	})
+	t.Run("Test Setting invalid option", func(t *testing.T) {
+		err := got.Set("INVALID")
+		assert.Error(t, err)
+		assert.Equal(t, "NOTSET", got.Type())
+	})
 
-	err := got.Set("INVALID")
-	assert.Error(t, err)
-	assert.Equal(t, "NOTSET", got.Type())
+	t.Run("Test String/Type/Get", func(t *testing.T) {
+		got = models.TESTING
+		assert.Equal(t, "TESTING", got.String())
+		assert.Equal(t, "TESTING", got.Type())
+	})
 }
 func TestGUIModeYAML(t *testing.T) {
-	// Test Mrashal and Unmarshal YAML
+	got := models.TESTING
+	t.Run("Test MarshalYAML", func(t *testing.T) {
+		got.MarshalYAML()
+	})
 }
